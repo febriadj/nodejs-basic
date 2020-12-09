16b1f93ddfb5cc9c49473a1ed628d0890862b728
i'mfebri <73212374+febriadj@users.noreply.github.com>
2020-12-10 01:02:51 +0700

    update server.js

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ const server = http.createServer((req, res)=> {
 })
 
 // running the server on port 3000
-server.listen(3000, 'localhost', () => {
-  console.log('listening for request on port 3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`listening for request on port ${port}`);
 })
